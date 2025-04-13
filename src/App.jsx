a11425015aac75d40ea6469aa5b5b27f292ab0eb
tundeani <tundeanimashaun15@gmail.com>
2025-04-13 16:20:57 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import FetchUserApp from './projects/fetchUser/FetchUserApp'
 import TimerApp from './projects/timer/TimerApp'
 import ToggleDarkModeApp from './projects/toggleDarkMode/ToggleDarkModeApp'
 import WeatherApp from './projects/weatherApp/WeatherApp'
+import './App.css';
 
 function App () {
   return (
-    <div style={{ padding: '20px'}}>
-      <nav>
-        <Link to= "/counter">Counter</Link>
-        <Link to= "/fetchUser">Fetch User</Link>
-        <Link to= "/timer">Timer</Link>
-        <Link to= "/toggleDarkMode">Toggle-Dark-Mode</Link>
-        <Link to= "/weatherApp">Weather-App</Link>
+    <div className="app-container">
+      <h1 className="app-title">React Projects Playground</h1>
+      <nav className="projects-list">
+        <Link to= "/counter" className="project-link">Counter</Link>
+        <Link to= "/fetchUser" className="project-link">Fetch User</Link>
+        <Link to= "/timer" className="project-link">Timer</Link>
+        <Link to= "/toggleDarkMode" className="project-link">Toggle-Dark-Mode</Link>
+        <Link to= "/weatherApp" className="project-link">Weather-App</Link>
       </nav>
 
       <Routes>

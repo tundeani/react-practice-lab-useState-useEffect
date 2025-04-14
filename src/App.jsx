ed6f3da92fe2fd08c16c2c8cedbf899311c1b2d3
tundeani <tundeanimashaun15@gmail.com>
2025-04-14 09:54:52 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,39 @@ import ToggleDarkModeApp from './projects/toggleDarkMode/ToggleDarkModeApp'
 import WeatherApp from './projects/weatherApp/WeatherApp'
 import './App.css';
 
+function HomePage() {
+  return (
+    <div className="home-content">
+      <h2>Welcome to React Projects Playground</h2>
+      <p>Select a project from the list above to get started.</p>
+    </div>
+  );
+}
+
 function App () {
   return (
     <div className="app-container">
       <h1 className="app-title">React Projects Playground</h1>
+      
       <nav className="projects-list">
-        <Link to= "/counter" className="project-link">Counter</Link>
-        <Link to= "/fetchUser" className="project-link">Fetch User</Link>
-        <Link to= "/timer" className="project-link">Timer</Link>
-        <Link to= "/toggleDarkMode" className="project-link">Toggle-Dark-Mode</Link>
-        <Link to= "/weatherApp" className="project-link">Weather-App</Link>
+        <Link to="/" className="project-link">Home</Link>
+        <Link to="/counter" className="project-link">Counter</Link>
+        <Link to="/fetchUser" className="project-link">Fetch User</Link>
+        <Link to="/timer" className="project-link">Timer</Link>
+        <Link to="/toggleDarkMode" className="project-link">Toggle Dark Mode</Link>
+        <Link to="/weatherApp" className="project-link">Weather App</Link>
       </nav>
 
       <Routes>
-        <Route path= "/counter" element= {<CounterApp />} />
-        <Route path= "/fetchUser" element= {< FetchUserApp />} />
-        <Route path= "/timer" element= {< TimerApp />} />
-        <Route path= "/toggleDarkMode" element= {< ToggleDarkModeApp />} />
-        <Route path= "/weatherApp" element= {< WeatherApp />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/counter" element={<CounterApp />} />
+        <Route path="/fetchUser" element={<FetchUserApp />} />
+        <Route path="/timer" element={<TimerApp />} />
+        <Route path="/toggleDarkMode" element={<ToggleDarkModeApp />} />
+        <Route path="/weatherApp" element={<WeatherApp />} />
       </Routes>
     </div>
-  )
-
-
+  );
 }
 
 export default App;
